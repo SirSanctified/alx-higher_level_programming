6dc8a41b1e86dcb35e2925641e37f75a4408d2fd
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-08-24 13:10:35 -0700

    Add 100-print-tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i - 32), end='')
+    print('{}'.format(chr(i) if i % 2 == 0 else chr(i - 32)), end='')

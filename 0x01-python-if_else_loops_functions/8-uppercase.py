9284c3fdd9b670c08bac3076d107e1b59a9a30cc
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-08-24 08:50:47 +0000

    Add 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    return ''.join([chr(ord(char) - 32) for char in str if ord(char) >= 65])
+    result = ''.join([chr(ord(char) - 32) for char in str if ord(char) >= 65])
+    print(result)

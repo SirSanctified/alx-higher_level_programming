6cec7120435a61d826d816d745a4fc41b5a71847
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-08-24 09:12:56 +0000

    Add 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
+    for i in range(1, 100):
         if i % 3 == 0:
             print('{}'.format('Fizz'), end=' ')
         elif i % 5 == 0:

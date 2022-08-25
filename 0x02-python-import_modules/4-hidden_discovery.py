f07e999ab89591e679576248788ce494e46aad0f
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-08-25 21:48:45 +0000

    Add 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == '__main__':
-    for i in dir(hidden_4):
+    for i in dir(hidden_4).sorted():
         if not i.startswith('__'):
             print(i)

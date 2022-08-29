76d49445ee5fa385e11db6d0b41101494103e251
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-08-29 16:52:26 +0000

    Add 9-max_integer.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(0, my_list):
+    new_list = []
+    for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
-            my_list[i] = True
+            new_list.append(True)
         else:
-            my_list[i] = False
-    return my_list
+            new_list.append(False)
+    return new_list
+

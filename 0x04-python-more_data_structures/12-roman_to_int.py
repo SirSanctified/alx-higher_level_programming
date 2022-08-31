9c1975ebf737a193cf9b6cfc3b234ae22ab3243a
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-08-31 18:22:19 +0000

    Add 11-multiply_list_map.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
-    for i in range(len(s)):
+    for i in range(len(roman_string)):
         if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
             int_val += rom_val[roman_string[i]] - 2 * rom_val[roman_string[i - 1]]
         else:

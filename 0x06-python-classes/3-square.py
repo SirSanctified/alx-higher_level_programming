28033ba7ed32c9d5bc41c571f9ff9b05b22de6bb
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-13 18:21:17 +0200

    Add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+this module is a based on 2-square.py, but contains type checking
+"""
+
+
+class Square:
+    """ Square class definition """
+    def __init__(self, size=0):
+        """ class initialization """
+        self.size = size
+
+    @property
+    def size(self):
+        """ getter """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ setter """
+        if not type(size) == int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """ calculate the area of this square """
+        return self.size * 4

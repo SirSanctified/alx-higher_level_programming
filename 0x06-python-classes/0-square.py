43f2dc23327932faa79d8b6f778d852a815b3b13
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-13 18:12:37 +0200

    Add 2-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+simple, empty squre class
+"""
 
 class Square:
+    """ empty body """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+square class based on 0-square.py
+"""
 
 class Square:
+    """ initialisation block """
     def __init__(self, size):
+        """ instance attributes """
         self.__size = size

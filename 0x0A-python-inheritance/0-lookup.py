c0fe98fc52719ece6e304d5ab860fd765a8dde6b
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-26 14:50:22 +0200

    Add 1-my_list.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,4 +10,3 @@ def lookup(obj):
         returns the list of available attributes and methods of an object
     """
     return list(dir(obj))
-
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    Define a function called print_sorted inside MyList class
+    which inherits from the built-in list class,
+    that prints the elements of MyList
+    in sorted ascending order
+"""
+
+
+class MyList(list):
+    """
+        Inherit the list class and extend its functionality
+    """
+    
+    def print_sorted(self):
+        """
+            Print this list in sorted order
+        """
+        nw = self.copy()
+        nw.sort()
+        print(nw)

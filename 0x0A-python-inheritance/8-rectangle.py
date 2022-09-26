6e6af8ae77425956bad79619c3819bf9eded3dad
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-26 16:58:55 +0200

    Fix 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 
 
-class Rectangle((BaseGeometry):
+class Rectangle(BaseGeometry):
     """
         Implement all the functionality of BaseGeometry
         Add extra functionality

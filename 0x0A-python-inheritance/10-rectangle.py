cc58d994a1b00176b151b5a836048ad5d124c797
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-26 17:23:23 +0200

    Add square.py

---
+++ 0x0A-python-inheritance/10-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+"""
+    Create a Rectangle class that inherits from BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+        Define the area method but do not implement it
+    """
+    def area(self):
+        """
+            Raise an exception since not yet implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+            Perform various checks to validate an integer supplied
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        if type(name) is not str:
+            raise TypeError("name must be a string")
+
+
+class Rectangle(BaseGeometry):
+    """
+        Implement all the functionality of BaseGeometry
+        Add extra functionality
+    """
+    def __init__(self, width, height):
+        """
+            initialise the rectangle object
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+            Implementing the area method
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+            string representation of Rectangle
+        """
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__width, self.__height)
+
+class Square(Rectangle):
+    """
+        Redefine the __init__ method (override)
+    """
+
+    def __init__(self, size):
+        """
+            Square objects initializer
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+            Override the area from base class
+        """
+        return size * size

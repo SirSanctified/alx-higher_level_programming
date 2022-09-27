53cf4c8b9b2c70c6bbda37e18657f7ed3624d674
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-27 17:10:43 +0200

    Add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    Read a file and print its contents to the stdout
+"""
+
+
+def read_file(filename=""):
+    """
+        Open and read the file
+    """
+    if filename:
+        with open(filename, 'r', encoding="utf-8") as file:
+            print(file.read())

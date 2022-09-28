f8eaccc990ff2e54534a2f3000f7d6f6bae1ee99
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-28 14:45:38 +0200

    4-from_json_string.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,4 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
         save @my_obj to @filename
     """
-    json.load(my_obj, filename)
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.load(my_obj, f)

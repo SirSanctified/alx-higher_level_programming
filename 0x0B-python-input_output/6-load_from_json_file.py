50e4f0f9674b97ed297f56bc9009d207846ced6a
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-28 14:55:37 +0200

    Add 5-save_to_json_file.py.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    load an object from text file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+        Deserialize a json object from @filename
+    """
+    
+    with open(filename, encoding='utf-8') as f:
+        return json.load(filename)

f4a94799f4e5ef88894f551285743dd259c45aa9
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-10-01 10:29:03 +0000

    Add base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    The class defined here will be the “base” of all other classes
+    in this project. The goal of it is to manage id attribute in all
+    future classes and to avoid duplicating the same code
+    (by extension, same bugs)
+"""
+
+
+class Base:
+    """
+        This class only handles the initialization part with the id attribute
+    """
+    def __init__(self, id=None):
+        """
+            Initialise the `id` with the number of instances if supplied id is
+            None
+        """
+        __nb_objects = 0
+        if not id:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id

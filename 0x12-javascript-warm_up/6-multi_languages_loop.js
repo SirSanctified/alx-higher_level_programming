71415a9d25631d29110d65e27d66bf90e75025f2
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-11-28 08:38:10 +0200

    Add 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// loop through an array
+
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let lang = 0; lang < langs.length; lang++) {
+  console.log(langs[lang]);
+}

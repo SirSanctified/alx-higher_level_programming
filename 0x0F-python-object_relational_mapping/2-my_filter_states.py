6f850ddee1c7efe302875c770921876b979f5d01
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-04 09:13:46 -0800

    Add 1-filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,32 +4,32 @@
     Filter states through user input
 """
 
-import sys
-import MySQLdb
+import sys
+import MySQLdb
 
 
-def main():
-    conn = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8"
-                           )
-    cur = conn.cursor()
-    search = sys.argv[4]
-    query = """SELECT * FROM states where name = '{:s}'
-            ORDER by id ASC""".format(search)
-    cur.execute(query)
-    row = cur.fetchall()
+def main():
+    conn = MySQLdb.connect(
+                        host='localhost',
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    search = sys.argv[4]
+    query = """SELECT * FROM states WHERE name = {:s}
+            ORDER BY id ASC""".format(search)
+    cur.execute(query)
+    row = cur.fetchall()
 
-    for r in row:
-        if r[1] == search:
-            print(r)
-    cur.close()
-    conn.close()
+    for r in row:
+        if r[1] == search:
+            print(r)
+    cur.close()
+    conn.close()
 
 
- if __name__ == "__main__":
-     main()
+if __name__ == '__main__':
+    main()

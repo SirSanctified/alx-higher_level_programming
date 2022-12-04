bf471c922d016e1ef56f0d8ac4d8fae0647c3c02
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-04 09:21:47 -0800

    Add 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+    Filter states through user input
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    conn = MySQLdb.connect(
+                        host='localhost',
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    search = sys.argv[4]
+    cur.execute('SELECT * FROM states WHERE name=%s
+            ORDER BY id ASC', (search,))
+    cur.execute(query)
+    row = cur.fetchall()
+
+    for r in row:
+        if r[1] == search:
+            print(r)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()

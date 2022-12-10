aa4e642cdc4d2e684a778b1b9f48f69931e26a27
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-10 03:02:05 +0200

    Update model-state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance Base = declarative_base()
+
+"""
+contains the class definition of a State and an instance Base = declarative_base()
 """
 
 from sqlalchemy.ext.declarative import declarative_base

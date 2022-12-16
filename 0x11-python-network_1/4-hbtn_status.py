aff44cfa92f885de54f3e25d695c410c1e853597
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-16 21:46:56 +0200

    Add 5-hbtn_header.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status using requests package
+"""
+
+import requests
+
+
+if __name__ == '__main__':
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body Response:')
+    print(f'\t - type: {type(res.text)}')
+    print(f'\t - content: {res.text}')
---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+takes in a URL,
+sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    print(requests.get(sys.argv[1]).headers['X-Request-Id'])

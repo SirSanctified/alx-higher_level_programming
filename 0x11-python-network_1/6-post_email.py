23221f03adc2b2be8f938e0a02f9aa5c0bf00758
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-16 21:57:14 +0200

    Add 5-hbtn_header.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    res = requests.post(url, data=email)
+    print(res.text)

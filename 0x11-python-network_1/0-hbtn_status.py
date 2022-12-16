915da623480e760e81eb9ba4212ff0388ef4cded
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-16 20:34:20 +0200

    Add 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status using urllib package
+"""
+
+
+import urllib.request
+
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as res:
+    data = res.read()
+    print('Body Response:')
+    print(f'\t- type: {type(data)}')
+    print(f'\t- content: {data}')
+    print(f'\t- utf8 content: {data.decode()}')

9f51239f35293be881ab59cabffbed1713ad2f13
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-17 08:42:29 +0200

    Fix 0-hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as res:
         data = res.read()
-        print('Body Response:')
+        print('Body response:')
         print(f'\t- type: {type(data)}')
         print(f'\t- content: {data}')
         print(f'\t- utf8 content: {data.decode()}')

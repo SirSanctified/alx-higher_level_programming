2cdc205ead3420962d27cac0473953c0eecf8f12
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-17 17:26:23 +0200

    Make 2-statuscode.js executable

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,9 +5,9 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response) {
+request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
     console.log('code:', response.statusCode);
   }

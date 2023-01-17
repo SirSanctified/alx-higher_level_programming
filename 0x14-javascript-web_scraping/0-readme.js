08da38e3c32728f482723c42567cf58f405de9a2
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-17 17:08:41 +0200

    Add script to read and print contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 // reads and prints the content of a file
 
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const filename = process.argv[2];
 
-fs.readFile(filename, 'utf-8')
-  .then((text) => {
+fs.readFile(filename, 'utf-8', (err, text) => {
+  if (err) {
+    console.log(err);
+  } else {
     process.stdout.write(text);
-  })
-  .catch((err) => console.log(err));
+  }
+});
